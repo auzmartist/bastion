f29fe52f4d66417f8a9d96498ef4c142241f8bdb
Austin Martin <auz.martin@gmail.com>
2015-05-16 13:25:38 -0500

    Add quick site files

---
+++ header.php
@@ -0,0 +1,6 @@
+<header>
+	<h1 class="text-center">Bastion</h1>
+	<div class="">
+		
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<meta charset="utf-8"></meta>
+		<title>Bastion</title>
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Comfortaa:400,300' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="style.css"></link>
+		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script src="site.js"></script>
+	</head>
+	<body>
+		<?php include 'header.php';?>
+		<?php include 'menu.php';?>
+		<?php include 'footer.php';?>
+	</body>
+</html>
